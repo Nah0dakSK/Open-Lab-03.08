9df9d8e87e56300772f92d4751ca51bc9c1f5281
Patrik Fatura <patofatura@gmail.com>
2020-10-30 13:23:50 +0100

    Done

--- Open-Lab-03.08/Checker.cs
+++ Open-Lab-03.08/Checker.cs
@@ -6,7 +6,22 @@ namespace Open_Lab_03._08
     {
         public bool IsPlural(string word)
         {
-            throw new NotImplementedException();
+            if(word.EndsWith("s"))
+                return true;
+            if(word.EndsWith("ies"))
+                return true;
+            if(word.EndsWith("es"))
+                return true;
+            if(word.EndsWith("uses"))
+                return true;
+            if(word.EndsWith("ses"))
+                return true;
+            if(word.EndsWith("ves"))
+                return true;
+            if(word.EndsWith("ices"))
+                return true;
+            else 
+                return false;
         }
     }
 }
